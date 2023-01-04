cb4fd570ecf508ae59a7cdc8497e5480dded2236
Anthony Cedrone <antcedrone95@gmail.com>
2023-01-04 13:24:35 -0500

    fixed new password route and dashboard is showing properly

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection.js');
 const path = require('path');
+const bcrypt = require('bcrypt');
 
 
 
