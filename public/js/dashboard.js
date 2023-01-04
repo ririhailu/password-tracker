7d2d4fa37265a4b3dad5fd17ce78251ca856a667
alex barlow <alex.barlow@rocketmail.com>
2023-01-04 17:14:05 -0500

    added js file for dashboard route logic

---
+++ public/js/dashboard.js
@@ -0,0 +1,18 @@
+const updateBtn = document.querySelector('#Update');
+const deleteBtn = document.querySelector('#Delete');
+const copyBtn = document.querySelector('#Copy');
+updateBtn.addEventListener('click', updatePassword);
+deleteBtn.addEventListener('click', deletePassword);
+copyBtn.addEventListener('click', copyPassword);
+
+async function updatePassword() {
+   
+};
+
+async function deletePassword() {
+    
+};
+
+async function copyPassword() {
+
+}
