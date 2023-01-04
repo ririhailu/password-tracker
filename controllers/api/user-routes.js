b20b3bbebc4ffff9a14f98108d61f925ec7b5aba
alex barlow <alex.barlow@rocketmail.com>
2023-01-04 11:48:29 -0500

    user-route refactor

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,53 +1,53 @@
 const router = require('express').Router();
 const { User } = require('../../models');
-
+const bcrypt = require('bcrypt');
+function requireLogin(req, res, next) {
+  if (!req.session.logged_in) {
+    res.status(401).json({ message: 'You must be logged in to access this route' });
+  } else {
+    next();
+  }
+}
+User.prototype.isValidPassword = async function(password) {
+  return bcrypt.compare(password, this.password);
+}
 router.post('/', async (req, res) => {
-
     try{
         const userData = await User.create(req.body);
-
         req.session.save(() => {
             req.session.user_id = userData.id;
             req.session.logged_in = true;
-
             res.status(200).json(userData);
         });
     } catch (err) {
         res.status(400).json(err);
     }
 });
-
 router.post('/login', async (req, res) => {
     console.log(req.body.userName);
     console.log(req.body.password);
     try {
-        const userData = await User.findOne ({ where: { user_name: req.body.userName }});
-
+        const userData = await User.findOne({ where: { user_name: req.body.userName }});
         if (!userData) {
-            res 
+            res
                 .status(400)
                 .json({ message: 'Incorrect username or password!'});
             return;
         }
-
-        const validPassword = await userData.checkPassword(req.body.password);
-
+        const validPassword = await userData.isValidPassword(req.body.password);
         if (!validPassword) {
             res
                 .status(400)
                 .json({ message: 'Incorrect email or password!'});
             return;
         }
-
         req.session.save(() => {
             req.session.user_id = userData.id;
             req.session.logged_in  = true;
-
             res.json({ user: userData, message: 'You successfully logged in!'})
         });
     } catch (err) {
         res.status(400).json(err);
     }
-})
-
+});
 module.exports = router;
