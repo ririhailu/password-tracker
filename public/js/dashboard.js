fdcf2bd52512076c65f9eb572216a3d908b868e8
alex barlow <alex.barlow@rocketmail.com>
2023-01-04 17:52:23 -0500

    added api route for passwords with get routes needed for deleting and updating per id

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -5,12 +5,24 @@ updateBtn.addEventListener('click', updatePassword);
 deleteBtn.addEventListener('click', deletePassword);
 copyBtn.addEventListener('click', copyPassword);
 
-async function updatePassword() {
+async function updatePassword(event) {
    
 };
 
-async function deletePassword() {
-    
+async function deletePassword(event) {
+    if (event.target.hasAttribute('data-id')) {
+    const id = event.target.getAttribute('data-id');
+
+    const response = await fetch(`/api/passwords/${id}`, {
+        method: 'DELETE',
+    });
+
+    if (response.ok) {
+        document.location.replace('profile');
+    } else {
+        alert('Failed to delete project');
+    }
+   }
 };
 
 async function copyPassword() {
