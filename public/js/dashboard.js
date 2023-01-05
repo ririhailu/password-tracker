adee081fb56525f6b60d5024dca2eefdc9c40379
alex barlow <alex.barlow@rocketmail.com>
2023-01-05 10:52:52 -0500

    fixed bug where only first delete button is being listened to

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,13 +1,11 @@
-const updateBtn = document.querySelector('#Update');
-const deleteBtn = document.querySelector('#Delete');
-const copyBtn = document.querySelector('#Copy');
-updateBtn.addEventListener('click', updatePassword);
-deleteBtn.addEventListener('click', deletePassword);
-copyBtn.addEventListener('click', copyPassword);
+// const updateBtn = document.querySelectorAll('.Update');
+const deleteBtn = document.querySelectorAll('.delete');
+const copyBtn = document.querySelectorAll('.Copy');
+// updateBtn.addEventListener('click', updatePassword);
 
-async function updatePassword(event) {
+// async function updatePassword(event) {
    
-};
+// };
 
 async function deletePassword(event) {
     console.log('delete');
@@ -26,6 +24,22 @@ async function deletePassword(event) {
    }
 };
 
-async function copyPassword() {
+for (let i = 0; i < deleteBtn.length; i++) {
+    deleteBtn[i].addEventListener('click', deletePassword)
+}
 
+async function copyPassword() {
+    console.log("hit");
+    const copyText = document.getElementById('password');
+
+    navigator.clipboard.writeText(copyText).then(function() {
+        alert('Copying to clipboard was successful!');
+      }, function(err) {
+        alert('Could not copy text. ', err);
+      });
+
+};
+
+for (let y = 0; y < copyBtn.length; y++) {
+    copyBtn[y].addEventListener('click', copyPassword)
 }
