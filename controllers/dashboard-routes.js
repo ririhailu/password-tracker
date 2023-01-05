3c38e7d1d114bdcbd5708f24b85f82c971cad2da
Anthony Cedrone <antcedrone95@gmail.com>
2023-01-05 09:52:54 -0500

    successfully encrypted passwords when adding new ones to dashboard

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Passwords } = require('../models');
+const crypto = require('crypto');
 
 router.get('/', async (req, res) => {
     Passwords.findAll({
@@ -9,7 +10,7 @@ router.get('/', async (req, res) => {
     ).then(passwordDB => {
         const password = passwordDB.map(password => password.get({ plain: true }));
         const loggedIn = req.session.logged_in
-        res.render('dashboard', {password, loggedIn});
+        res.render('dashboard', { password, loggedIn });
     }).catch(err => {
         console.log(err);
         res.status(500).json(err);
@@ -22,24 +23,57 @@ router.get('/new', (req, res) => {
 
 router.post('/new', async (req, res) => {
     console.log(req.body);
+
+    const algorithm = "aes-256-cbc";
+
+    // generate 16 bytes of random data
+    const initVector = crypto.randomBytes(16);
+
+    // protected data
+    const message = req.body.password;
+
+    // secret key generate 32 bytes of random data
+    const securityKey = crypto.randomBytes(32);
+
+    // the cipher function
+    const cipher = crypto.createCipheriv(algorithm, securityKey, initVector);
+
+    // encrypt the message
+    // input encoding
+    // output encoding
+    let encryptedData = cipher.update(message, "utf-8", "hex");
+
+    encryptedData += cipher.final("hex");
+
+    console.log("Encrypted message: " + encryptedData);
+
+    // the decipher function
+    // const decipher = crypto.createDecipheriv(algorithm, securityKey, initVector);
+
+    // let decryptedData = decipher.update(encryptedData, "hex", "utf-8");
+
+    // decryptedData += decipher.final("utf8");
+
+    // console.log("Decrypted message: " + decryptedData);
+
     try {
         const newPassword = await Passwords.create({
             username: req.body.username,
             title: req.body.title,
-            password: req.body.password,
-            initVector: req.body.initVector,
-            securityKey: req.body.securityKey,
+            password: encryptedData,
+            initVector: JSON.stringify(initVector),
+            securityKey: JSON.stringify(securityKey),
             user_id: 1,
         });
         // const newPassword = await Passwords.create({
         //   ...req.body,
         //   user_id: req.session.user_id,
         // });
-    
+
         res.status(200).json(newPassword);
-      } catch (err) {
+    } catch (err) {
         res.status(400).json(err);
-      }
+    }
 })
 
 // logoutRouter.get('/', (req, res) => {
